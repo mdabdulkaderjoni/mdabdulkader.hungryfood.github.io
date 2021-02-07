4eebdfb18bd8612e5e42f7cbf5851d8d3e2ab224
mdabdulkaderjoni <mdjoniemail@gmail.com>
2021-02-07 20:40:24 +0600

    developed functionality

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
+ const reSetAll=()=> {
+    document.getElementById('mainDiv').style.display = 'grid';
+    document.getElementById('mainDiv').innerHTML = "";
+    document.getElementById('subDiv').style.display = 'none';
+}
 
+//search-button event listener
 const submit = document.getElementById('submit-btn');
-submit.addEventListener('click',()=>{
-    document.getElementById('mainDiv').style.visibility='visible';
-    document.getElementById('mainDiv').innerHTML="";
-    document.getElementById('subDiv').style.display='none';
-    
-   
+submit.addEventListener('click', () => {
+    //this function will reset everything after search clicked
+    reSetAll(); 
 
     let input = document.getElementById('getName').value;
     let getName = input[0];
- 
-    if (input.length == 1) {
+
+    if(input.length==0){
+        swal("Nothing Found!", "Please Search Agian!", "info");
+    }
+    else if (input.length == 1) {
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${getName}`)
             .then(res => res.json())
             .then(data => displayItems(data))
-            
-
     }
     else {
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
@@ -26,15 +30,17 @@ submit.addEventListener('click',()=>{
 
 
     let displayItems = items => {
+        if(items.meals==null){
+            swal("Invalid Item Name!", "Please Search Agian!", "info");
+
+        }
         const mainDiv = document.getElementById('mainDiv');
 
         for (let item in items.meals) {
-            //console.log(items.meals[item].strMeal)
 
             const itemDiv = document.createElement('div');
             itemDiv.className = 'itemDiv';
             itemDiv.setAttribute("id", "itemDiv");
-
 
             const itemInfo = `
                 <h3>${items.meals[item].strMeal}</h3>
@@ -43,37 +49,47 @@ submit.addEventListener('click',()=>{
             itemDiv.innerHTML = itemInfo;
             mainDiv.appendChild(itemDiv);
 
-            //another event
-            itemDiv.addEventListener('click',()=>{
-                document.getElementById('mainDiv').style.visibility='hidden';
-                document.getElementById('subDiv').style.display='block';
-
-                const itemDiv = document.createElement('div');
-            itemDiv.className = 'itemInfo';
-            itemDiv.setAttribute("id", "itemInfo");
-
-
-            const itemInfo = `
+            //calling showInfo for info event 
+                (()=>{
+                    itemDiv.addEventListener('click', () => {
+                        document.getElementById('mainDiv').style.display = 'none';
+                        document.getElementById('subDiv').style.display = 'block';
                 
-                <img src="${items.meals[item].strMealThumb}">
-                <h3>Item Name: ${items.meals[item].strMeal}</h3>
-                <p>Category: ${items.meals[item].strCategory}</p>
-                <p>Area: ${items.meals[item].strArea}</p>
-                `;
-            itemDiv.innerHTML = itemInfo;
-            subDiv.appendChild(itemDiv);
+                        const itemDiv = document.createElement('div');
+                        itemDiv.className = 'itemInfo';
+                        itemDiv.setAttribute("id", "itemInfo");
+                        
+                        let incredient=items.meals[item];
+                        const itemInfo = `
+                        
+                        
+                        <img src="${items.meals[item].strMealThumb}">
+                        <h2>${incredient.strMeal}</h2>
+                        <ul>
+                        <li>${incredient.strMeasure1} ${incredient.strIngredient1}</li>
+                        <li>${incredient.strMeasure2} ${incredient.strIngredient2}</li>
+                        <li>${incredient.strMeasure3} ${incredient.strIngredient3}</li>
+                        <li>${incredient.strMeasure4} ${incredient.strIngredient4}</li>
+                        <li>${incredient.strMeasure5} ${incredient.strIngredient5}</li>
+                        <li>${incredient.strMeasure6} ${incredient.strIngredient6}</li>
+                        <ul>
 
-
-
-            })
-
+                        
+                      
+                        `;
+                        itemDiv.innerHTML = itemInfo;
+                        subDiv.appendChild(itemDiv);
+                
+                
+                
+                    })
+                })()
+         
         }
-
     }
-
-    
 })
 
 
-    
-    
+
+
+
